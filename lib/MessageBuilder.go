1913929ba3429d796f48cc47da5bc2caa0803c41
Iain Munro <iain17@me.com>
2016-08-10 22:24:23 +0100

    Fix tests that called message.Received

--- lib/MessageBuilder.go
+++ lib/MessageBuilder.go
@@ -11,13 +11,12 @@ func build(definition reflect.Type, jsonData string) interfaces.IMessage {
 	messageInterface := reflect.New(definition).Interface()
 	err := json.Unmarshal([]byte(jsonData), &messageInterface)
 	if err != nil {
-		log.Print("Error unmarshalling the message", err)
+		log.Print("Error unmarshalling the message. This is probably due to the message json not being conform to the message struct:", err)
 		return nil
 	}
 	if message, ok := reflect.ValueOf(messageInterface).Elem().Interface().(interfaces.IMessage); ok {
 		return message
 	}
-
 	log.Print("Could not cast message to IMessage interface. Invalid MessageType")
 	return nil
 }
--- lib/MessageMethodResult_test.go
+++ lib/MessageMethodResult_test.go
@@ -7,7 +7,7 @@ func TestMessageMethodResult_Received(t *testing.T) {
 	parameters = append(parameters, "1")
 	parameters = append(parameters, 2)
 	message := newMessageMethodResult(true, parameters)
-	result := message.Received()
+	result := message.Received(nil)
 	if result != nil {
 		t.Fatal(result)
 	}
